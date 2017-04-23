75352a4db18ea27e21de4a35405b42fdf3b7c5de
Binghui Liu <bertieliu52@gmail.com>
2017-04-23 17:08:21 +0300

    add increment function

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TouchableHighlight,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ActionCreators } from '../actions';
 
 class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { recipeCount: 0 }
+  }
+
+  incrementRecipeCount() {
+    this.setState({ recipeCount: this.state.recipeCount + 1 });
+  }
+
   render() {
     return (
       <View>
         <Text style = {{marginTop: 20}}>
-         I am App Container!
+         I am App Container! Recipe Count: { this.state.recipeCount }
         </Text>
+        <TouchableHighlight onPress = {() => { this.incrementRecipeCount() }}>
+          <Text>Add Recipe</Text>
+        </TouchableHighlight>
       </View>
     );
   }
