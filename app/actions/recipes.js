ecaff4217dee4c8deea254302f957690b94c990a
Binghui Liu <bertieliu52@gmail.com>
2017-04-23 21:47:00 +0300

    fetch recipes from food puppy api

--- app/actions/recipes.js
+++ app/actions/recipes.js
@@ -1,4 +1,5 @@
 import * as types from './types';
+import Api from '../lib/api';
 
 export function addRecipe() {
   return {
@@ -7,5 +8,15 @@ export function addRecipe() {
 }
 
 export function fetchRecipes(ingredients) {
-  
+  return (dispatch, getState) => {
+    const params = [
+      `i=${encodeURIComponent(ingredients)}`,
+      'p=3'
+    ].join('&');
+    return Api.get(`/api/?${params}`).then(resp => {
+      console.log(resp);
+    }).catch( (ex) => {
+      console.log(ex);
+    });
+  }
 }
